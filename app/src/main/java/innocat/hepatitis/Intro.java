b6dedea9407e29bbd521c0f32ed0b6cd7319116d
bamwine <33618467+bamwine@users.noreply.github.com>
2023-02-06 04:33:04 -0800

    Initial commit

---
+++ app/src/main/java/innocat/hepatitis/Intro.java
@@ -0,0 +1,13 @@
+package innocat.hepatitis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Intro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
